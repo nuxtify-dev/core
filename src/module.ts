1b7810ebbb983eef300243bf096d54790d832670
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-05-13 17:08:52 -0400

    feat: add nuxtify scaffolding

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,12 @@
-import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import {
+  addComponentsDir,
+  addImportsDir,
+  addServerImportsDir,
+  createResolver,
+  defineNuxtModule,
+  installModule,
+} from '@nuxt/kit'
+import { defu } from 'defu'
 
 // Types
 import type { ModuleOptions } from './types'
@@ -57,10 +65,54 @@ export default defineNuxtModule<ModuleOptions>({
       support: '',
     },
   },
-  setup(_options, _nuxt) {
+  async setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    // Modules
+    await installModule('vuetify-nuxt-module', {
+      vuetifyOptions: {
+        icons: {
+          defaultSet: 'mdi-svg',
+        },
+        theme: {
+          themes: {
+            light: {
+              colors: {
+                primary: '#020420',
+                secondary: '#00DC82',
+                background: '#fff',
+              },
+            },
+          },
+        },
+        defaults: {
+          VBtn: { color: 'secondary', variant: 'flat', class: 'text-none' },
+          VAlert: {
+            VBtn: { color: 'inherit' },
+          },
+          VFooter: {
+            VBtn: { color: 'inherit' },
+          },
+        },
+      },
+    })
+
+    // Expose module options to app config
+    _nuxt.options.appConfig.nuxtify = defu(_nuxt.options.appConfig.nuxtify, {
+      ..._options,
+    })
+
+    // Components
+    addComponentsDir({
+      path: resolver.resolve('./runtime/components'),
+    })
+
+    // Composables
+    addImportsDir(resolver.resolve('./runtime/composables'))
+    addServerImportsDir(resolver.resolve('./runtime/server/composables'))
+
+    // Utils
+    addImportsDir(resolver.resolve('./runtime/utils'))
+    addServerImportsDir(resolver.resolve('./runtime/server/utils'))
   },
 })
