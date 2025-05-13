dcaf8d27001d1d59461c4906d3c417c395fd8e49
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-05-13 19:41:29 -0400

    dx: add playground nuxtify config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,15 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
-  nuxtifyCore: {},
+  nuxtifyCore: {
+    brand: {
+      name: '@nuxtify/core playground',
+    },
+    announcement: {
+      show: true,
+      message: 'Welcome to the @nuxtify/core playground!',
+      buttonText: 'Click me',
+      buttonUrl: 'https://nuxtify.dev/',
+    },
+  },
 })
