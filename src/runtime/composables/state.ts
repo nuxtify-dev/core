b231351bfee707b8e5e48bb2c7bbc4a58f3ce8c6
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-05-15 08:41:46 -0400

    feat: add useDrawer composable

--- src/runtime/composables/state.ts
+++ src/runtime/composables/state.ts
@@ -1,6 +1,7 @@
 import { ref, useState } from '#imports'
 
 // App
+export const useDrawer = () => useState<boolean | null>('drawer', () => null)
 export const useErrorMessage = () => useState('errorMessage', () => '')
 export const useToast = () =>
   useState('toast', () =>
