5dd9bd14efe14a2c34bf85c64ba60a863753974f
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-08-02 13:52:45 -0400

    playground: add brand domain

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -3,7 +3,8 @@ export default defineAppConfig({
     // Brand
     brand: {
       name: '@nuxtify/core',
-      tagline: 'Catchy tagline for tne @nuxtify/core module.',
+      domain: 'nuxtify.dev',
+      tagline: 'Catchy tagline for the @nuxtify/core module.',
       logo: {
         lightUrl: '/img/nuxtify-logo-light.png', // Recommended 5:1 aspect ratio (e.g. 400 x 80 px)
         darkUrl: '/img/nuxtify-logo-dark.png', // Recommended 5:1 aspect ratio (e.g. 400 x 80 px)
